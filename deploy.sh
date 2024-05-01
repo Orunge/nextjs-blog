e142ff3ca6f197db4e3fc6f5f00d9219f950784c
bteg <solongotaishir@gmail.com>
2024-05-01 15:16:53 +0000

    path

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 npm run build  # Build your Next.js application
-rsync -avz --exclude="node_modules" ./out/ nextjs/nextjs-blog  # Adjust paths if needed
+rsync -avz --exclude="node_modules" ./next/ nextjs/nextjs-blog  # Adjust paths if needed
